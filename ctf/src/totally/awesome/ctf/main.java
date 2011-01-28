1e87122306697c4f6c3d99001a5f33ec5b49c1d4
cscpenguin <cscpenguin@3300ae4f-8763-6a4b-1a40-708f717647f1>
2011-01-28 19:17:16 +0000

    Sign in - goes to created Sign In Page Create a char - nothing Exit - exits

--- ctf/src/totally/awesome/ctf/main.java
+++ ctf/src/totally/awesome/ctf/main.java
@@ -1,7 +1,10 @@
 package totally.awesome.ctf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,38 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button signin = (Button)findViewById(0x7f050000); //id is button 1
+        signin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//requestWindowFeature(Window.FEATURE_NO_TITLE);
+				Intent i = new Intent();
+				i.setClassName("totally.awesome.ctf", "totally.awesome.ctf.signin");
+				startActivity(i);
+			}
+		});
+        
+        Button createchar = (Button)findViewById(0x7f050001); //id is button 2
+        createchar.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//requestWindowFeature(Window.);
+			}
+		});
+        
+        Button exitb = (Button)findViewById(0x7f050002); //id is button 3
+        exitb.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//requestWindowFeature(Window.);
+				finish();
+			}
+		});
     }
 }
---
+++ ctf/src/totally/awesome/ctf/signin.java
@@ -0,0 +1,28 @@
+package totally.awesome.ctf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class signin extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+        Button back = (Button)findViewById(0x7f05000a);
+        back.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//requestWindowFeature(Window.FEATURE_NO_TITLE);
+				Intent i = new Intent();
+				i.setClassName("totally.awesome.ctf", "totally.awesome.ctf.main");
+				startActivity(i);
+			}
+		});
+    }
+}
