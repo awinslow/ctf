867c5f3637c2beca53e8df205f6ebe0af95b158a
glazier.samuel@gmail.com <glazier.samuel@gmail.com@3300ae4f-8763-6a4b-1a40-708f717647f1>
2011-02-10 21:47:07 +0000

    Stats Initialize correctly for battle

--- trunk/ctf/src/totally/awesome/ctf/fight.java
+++ trunk/ctf/src/totally/awesome/ctf/fight.java
@@ -1,5 +1,17 @@
 package totally.awesome.ctf;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 public class fight {
 	int myHealth;
 	int enemyHealth;
@@ -10,14 +22,109 @@ public class fight {
 	int myMaxHealth;
 	int enemyMaxHealth;
 	
+	int myAttackVal;
+	int enemyAttackVal;
+	
+	int myDefenseVal;
+	int enemyDefenseVal;
+	
 	boolean myTurn;
 	
 	fight(int eid){
-		//myMaxHealth = m;
-		//enemyMaxHealth = e;
+		
 		enemyID = eid;
 		myName = info.theAuth.name;
 		myId = info.theAuth.id;
+		
+		InitializeStats(true);//Enemy Stats
+		InitializeStats(false);//My Stats
+		
+
+	}
+	
+	void InitializeStats(boolean isEnemy)
+	{
+		try {
+			String text = "";
+			String inputLine;
+			
+			//ME
+			
+			URL u;
+			if(!isEnemy)
+			{
+				u = new URL("http://ctf.awins.info/stats.php?uid=" + Integer.toString(myId));
+			}
+			else
+			{
+				u = new URL("http://ctf.awins.info/stats.php?uid=" + Integer.toString(enemyID));
+			}
+
+			HttpURLConnection h = (HttpURLConnection) u.openConnection();
+			h.setRequestMethod("GET");
+			h.connect();
+			if(h.getResponseCode()==200){
+
+				BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                        h.getInputStream()));
+				
+				while ((inputLine = in.readLine()) != null)
+				{
+				    text=inputLine;
+				    
+				    if(text.indexOf("Health:") != -1)
+				    {
+				    	if(!isEnemy)
+				    		myMaxHealth = (Integer.parseInt(text.substring(7).trim()));
+				    	else
+				    		enemyMaxHealth = (Integer.parseInt(text.substring(7).trim()));
+
+				    }
+				    else if(text.indexOf("Attack:") != -1)
+				    {
+				    	if(!isEnemy)
+				    		myAttackVal = Integer.parseInt(text.substring(7).trim());
+				    	else
+				    		enemyAttackVal = Integer.parseInt(text.substring(7).trim());
+
+				    }
+				    else if(text.indexOf("Defense:") != -1)
+				    {
+				    	if(!isEnemy)
+				    		myDefenseVal = Integer.parseInt(text.substring(8).trim());
+				    	else
+				    		enemyDefenseVal = Integer.parseInt(text.substring(8).trim());
+
+				    }
+				    else if(text.indexOf("Username:") != -1)
+				    {
+				    	if(!isEnemy)
+				    		myName = text.substring(10);
+				    	else
+				    		enemyName = text.substring(10);
+
+				    }
+				    else
+				    {
+				    	//Not parsing correctly
+				    }
+				}
+				in.close();
+			}
+			else{
+				
+				Log.i("Fight initialization error:", "Response code NOT 200!!! for ME");
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.i("URL BREAKING SHIT", "Malformed URL");
+		} catch (IOException e){
+			e.printStackTrace();
+			Log.i("IO", "IO exception in fight constructor");
+		}
 	}
 	
 	void setMyMaxHealth(int h)
