7afd6dca46a4cbf9071a4dd8506badbeb02c33c4
glazier.samuel@gmail.com <glazier.samuel@gmail.com@3300ae4f-8763-6a4b-1a40-708f717647f1>
2011-02-17 23:00:31 +0000

    Added Player Classes

---
+++ trunk/ctf/src/totally/awesome/ctf/Hacker.java
@@ -0,0 +1,33 @@
+package totally.awesome.ctf;
+
+public class Hacker extends Player {
+	
+	public Hacker()
+	{
+		
+	}
+
+	@Override
+	void attack1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void attack2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void attack3() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	String getName()
+	{
+		return "Hacker";
+	}
+}
---
+++ trunk/ctf/src/totally/awesome/ctf/Soldier.java
@@ -0,0 +1,33 @@
+package totally.awesome.ctf;
+
+public class Soldier extends Player {
+
+	public Soldier() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void attack1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void attack2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void attack3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	String getName()
+	{
+		return "Soldier";
+	}
+
+}
