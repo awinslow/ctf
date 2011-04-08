4f03eef00931c91704703ed8420a5eb3aec010e2
glazier.samuel <glazier.samuel@gmail.com>
2011-04-08 06:30:16 +0000

    More Sounds, Added explosion bmp, working on making it an animation.  Need more sounds still for other attacks.

--- ctf/src/totally/awesome/ctf/Hacker.java
+++ ctf/src/totally/awesome/ctf/Hacker.java
@@ -1,5 +1,9 @@
 package totally.awesome.ctf;
 
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+
 public class Hacker extends Player {
 	
 	public Hacker()
@@ -12,6 +16,26 @@ public class Hacker extends Player {
 	@Override
 	boolean attack1(int type) {
 		return super.attack1(getNumber());
+	}
+	
+	void soundAttack1(){
+		
+	}
+	void soundAttack2(){
+		
+	}
+	void soundAttack3(){
+		MediaPlayer mp = MediaPlayer.create(info.battleInst, R.raw.hacking);
+		try {
+			mp.prepare();
+		} catch (IllegalStateException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        mp.start();
 	}
 
 	@Override
--- ctf/src/totally/awesome/ctf/Sentinel.java
+++ ctf/src/totally/awesome/ctf/Sentinel.java
@@ -1,5 +1,9 @@
 package totally.awesome.ctf;
 
+import java.io.IOException;
+
+import android.media.MediaPlayer;
+
 public class Sentinel extends Player {
 	
 	public Sentinel()
@@ -13,7 +17,25 @@ public class Sentinel extends Player {
 	boolean attack1(int type) {
 		return super.attack1(getNumber());
 	}
-
+	void soundAttack1(){
+		MediaPlayer mp = MediaPlayer.create(info.battleInst, R.raw.powerup);
+		try {
+			mp.prepare();
+		} catch (IllegalStateException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        mp.start();
+	}
+	void soundAttack2(){
+		
+	}
+	void soundAttack3(){
+		
+	}
 	@Override
 	boolean attack2(int type) {
 		// TODO Auto-generated method stub
