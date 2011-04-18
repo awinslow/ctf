ab7f219fc93380f72267a970e9dcb077762192d5
rock488835 <rock488835@3300ae4f-8763-6a4b-1a40-708f717647f1>
2011-04-18 06:19:47 +0000

    you can now pickup items in adventure mode.  also excellent doesnt loop on my phone now

--- ctf/src/totally/awesome/ctf/adventure.java
+++ ctf/src/totally/awesome/ctf/adventure.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Canvas;
@@ -49,15 +51,111 @@ class InterestingLocations extends ItemizedOverlay<OverlayItem>{
 
 	  @Override
 	  protected boolean onTap(int index) {
-	    OverlayItem item = locations.get(index);
+	    final OverlayItem item = locations.get(index);
 	    /*AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
 	    dialog.setTitle(item.getTitle());
 	    dialog.setMessage(item.getSnippet());
 	    dialog.show();*/
+	    if(item.getSnippet().contains("item")){
+	    	String name = "that shouldnt happen";
+	    	String description = "that shouldnt happen";
+	    	HttpURLConnection h;
+			try {
+				URL u = new URL("http://ctf.awins.info/item.php?red=1&token="+info.theAuth.getToken()+"&stats=1&iid="+item.getTitle());
+				//Log.i("UPDATING LOCATION", "http://ctf.awins.info/checkin.php?&token="+info.theAuth.getToken()+"&lat="+locationManager.getLastKnownLocation(provider).getLatitude()+"&lon"+locationManager.getLastKnownLocation(provider).getLongitude());
+				h = (HttpURLConnection) u.openConnection();
+				h.setRequestMethod("GET");
+				h.connect();
+				if(h.getResponseCode()==200){
+					BufferedReader in = new BufferedReader(
+	                        new InputStreamReader(
+	                        h.getInputStream()));
+					String inputLine;
+					
+					while ((inputLine = in.readLine()) != null){
+						name = inputLine;
+						description = in.readLine();
+					}
+					    
+					in.close();
+					Log.i("LOCATION", "update good");
+				}
+				else{
+
+					Log.i("LOCATION", "update failed");
+					
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	
+ 
+		    
+			final AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+	        alert.setTitle("Pickup Item");
+	        alert.setMessage("Would you like to pick up the "+name+ " which "+description+"?");
+	        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	            public void onClick(DialogInterface dialog, int whichButton) {
+	            	Log.i("getItem", item.getSnippet());
+	            	int latStart = item.getSnippet().indexOf("lat:");
+	            	int lonStart = item.getSnippet().indexOf("lon:");
+	            	int latEnd = item.getSnippet().indexOf(" ", latStart);
+	            	//int lonEnd = item.getSnippet().indexOf(" ", lonStart);
+	            	String lat = item.getSnippet().substring(latStart+4, latEnd);
+	            	Log.i("getItem", lat);
+	            	String lon = item.getSnippet().substring(lonStart+4);
+	            	
+	            	
+	            	Log.i("getItem", lon);
+	            	
+	    	    	HttpURLConnection h;
+	    			try {
+	    				URL u = new URL("http://ctf.awins.info/item.php?red=1&token="+info.theAuth.getToken()+"&pickup=1&iid="+item.getTitle()+"&lat="+lat+"&lon="+lon);
+	    				Log.i("ITEM SHIT", "http://ctf.awins.info/item.php?red=1&token="+info.theAuth.getToken()+"&pickup=1&iid="+item.getTitle()+"&lat="+lat+"&lon="+lon);
+	    				h = (HttpURLConnection) u.openConnection();
+	    				h.setRequestMethod("GET");
+	    				h.connect();
+	    				if(h.getResponseCode()==200){
+	    					BufferedReader in = new BufferedReader(
+	    	                        new InputStreamReader(
+	    	                        h.getInputStream()));
+	    					String inputLine;
+	    					
+	    					while ((inputLine = in.readLine()) != null){
+	    						//name = inputLine;
+
+	    					}
+	    					    
+	    					in.close();
+	    					Log.i("LOCATION", "update good");
+	    				}
+	    				else{
+
+	    					Log.i("LOCATION", "update failed");
+	    					
+	    				}
+	    			} catch (IOException e) {
+	    				// TODO Auto-generated catch block
+	    				e.printStackTrace();
+	    			}
+	            }
+	        });
+
+	        alert.setNegativeButton("No",
+	                new DialogInterface.OnClickListener() {
+	                    public void onClick(DialogInterface dialog, int whichButton) {
+	                        dialog.cancel();
+	                    }
+	                });
+	        alert.show();	
+		    
+	    }else{
 		    info.checkingOutPlayer = Integer.parseInt(item.getTitle());
 			Intent i = new Intent();
 			i.setClassName("totally.awesome.ctf", "totally.awesome.ctf.adventureperson");
 			mContext.startActivity(i);
+	    }
 	    return true;
 	  }
 	  
@@ -218,14 +316,18 @@ public class adventure extends MapActivity{
 					String inputLine;
 					
 					while ((inputLine = in.readLine()) != null){
+						Log.i("adding item", inputLine);
+						String la, lo;
 						String name = inputLine;
 						inputLine = in.readLine();
+						la = inputLine;
 						int lat = (int) (Float.parseFloat(inputLine.substring(5))*1000000); //(degrees * 1E6)
 						Log.i("LOCATION", "lat: "+lat);
 						inputLine = in.readLine();
+						lo = inputLine;
 						int lon = (int) (Float.parseFloat(inputLine.substring(5))*1000000); //(degrees * 1E6)
 						Log.i("LOCATION", "lon: "+lon);
-						itemsOverlay.addOverlay(new OverlayItem(new GeoPoint(lat, lon), "Itemid: "+name, "FINISH HIM!!!"));
+						itemsOverlay.addOverlay(new OverlayItem(new GeoPoint(lat, lon), name.substring(5), "item"+" lat:"+la.substring(5)+ " lon:"+lo.substring(5)));
 					}
 					    
 					in.close();
